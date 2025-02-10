c0286f7d3701a5a1ce68449e1e9ff2b9eae75ed7
austin-zickur <zickuraustin.work@gmail.com>
2025-02-09 19:17:47 -0600

    initial goal

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import streamlit as st
+
+'''
+Goal:
+Use Friedmann equations to give a plot of the Hubble Constant
+with respect to time with sliders for the k (curvature),
+omega (density), and 
